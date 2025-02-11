f08ce5de0f65bbefa76bd2236206d9a30af332ad
mohamedelmougy <mohamed.elmougy11@gmail.com>
2025-02-11 12:13:58 +0200

    refactor: make container in specific className

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import AboutSection from "./AboutSection";
 
 const Hero = () => {
   return (
-    <div className="px-5 my-2">
+    <div className="container">
       <div className="flex gap-3 flex-col lg:flex-row ">
         <Card />
 
